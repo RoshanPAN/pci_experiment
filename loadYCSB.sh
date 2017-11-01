144447ca603b3927cc10c549c9f902c4c92b121b
root <root@CSE-Hcse101389D.(none)>
2017-11-01 19:47:16 -0400

    add new script to edit YCSB.

---
+++ loadYCSB.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+loadname="workloadmy"
+/usr/local/YCSB/bin/ycsb load hbase12 -P workloads/${loadname} -cp /usr/local/hbase-1.2.6/conf -p table=usertable -p columnfamily=family
+echo "run ${loadname}"
