c26fa2b1638dfddef3bfaa620fae58fd13d4aaa0
guihaomin <31326960+guihaomin@users.noreply.github.com>
2017-11-04 09:44:43 +0800

    Update loadYCSB.sh

--- loadYCSB.sh
+++ loadYCSB.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-loadname="workloadmy"
+loadname="workloada"
 /usr/local/YCSB/bin/ycsb load hbase12 -P workloads/${loadname} -cp /usr/local/hbase-1.2.6/conf -p table=usertable -p columnfamily=family
 echo "run ${loadname}"
