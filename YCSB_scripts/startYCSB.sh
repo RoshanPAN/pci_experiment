9a17b6d765920a61bcf2ad40abea7095b11bd91f
Luoshang Pan <panluoshang@live.com>
2017-11-07 09:50:33 -0500

    Copy the throughput log into docker container volume when finish YCSB

--- YCSB_scripts/startYCSB.sh
+++ YCSB_scripts/startYCSB.sh
@@ -4,3 +4,5 @@ cd $YCSB_PREFIX
 bin/ycsb run hbase12 -P workloads/${workname} -cp $HBASE_PREFIX/conf -p table=usertable -p columnfamily=family
 cd -
 echo "run ${workname}"
+cp /usr/local/YCSB.log /usr/local/pci_experiment_volume/YCSB_throughput.log && \
+	printf "\n YCSB.log copied into volume /usr/local/pci_experiment_volume "
