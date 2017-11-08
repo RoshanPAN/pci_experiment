b85f34d366552cb9e1ce071141802920900a4ea8
Luoshang Pan <panluoshang@live.com>
2017-11-08 00:28:43 -0500

    1. Add YCSB threads to 4  2. change workload

--- YCSB_scripts/startYCSB.sh
+++ YCSB_scripts/startYCSB.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 workname="workloady"
 cd $YCSB_PREFIX
-bin/ycsb run hbase12 -P workloads/${workname} -cp $HBASE_PREFIX/conf -p table=usertable -p columnfamily=family
+bin/ycsb run hbase12 -P workloads/${workname} -cp $HBASE_PREFIX/conf -p table=usertable -p columnfamily=family -thread 4
 cd -
 echo "run ${workname}"
 cp /usr/local/YCSB.log /usr/local/pci_experiment_volume/YCSB_throughput.log && \
