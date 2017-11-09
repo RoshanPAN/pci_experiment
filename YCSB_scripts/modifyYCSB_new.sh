51d976f1a07d803a0bed176064b7f9ce79b6e3bc
guihaomin <31326960+guihaomin@users.noreply.github.com>
2017-11-10 06:04:40 +0800

    Update modifyYCSB_new.sh

--- YCSB_scripts/modifyYCSB_new.sh
+++ YCSB_scripts/modifyYCSB_new.sh
@@ -1,4 +1,4 @@
 cp ./ycsbClientModified_new.java /usr/local/YCSB/core/src/main/java/com/yahoo/ycsb/Client.java
-cp ./ycsbModified /usr/local/YCSB/bin/ycsb
+cp ./ycsb_origin /usr/local/YCSB/bin/ycsb
 cp ./workloady $YCSB_PREFIX/workloads/workloady
 printf " File replaced.\n workloady added. \n YCSB will now log throughput into /usr/local/YCSB.log\n"
