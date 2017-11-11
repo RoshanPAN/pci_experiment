cc753a831700e92bfd354dd7d7113de7df119cd5
Luoshang Pan <panluoshang@live.com>
2017-11-11 16:17:37 -0500

    Print satus while loading

--- YCSB_scripts/loadYCSB.sh
+++ YCSB_scripts/loadYCSB.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 loadname="workloady"
 cd $YCSB_PREFIX
-$YCSB_PREFIX/bin/ycsb load hbase12 -P $YCSB_PREFIX/workloads/${loadname} -cp $HBASE_PREFIX/conf -p table=usertable -p columnfamily=family
+$YCSB_PREFIX/bin/ycsb load hbase12 -P $YCSB_PREFIX/workloads/${loadname} -cp $HBASE_PREFIX/conf -p table=usertable -p columnfamily=family -s
 cd -
 echo "run ${loadname}"
