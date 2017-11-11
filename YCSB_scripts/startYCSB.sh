726c97374e3630e322d90f3b4fcc73176551fbdf
Luoshang Pan <panluoshang@live.com>
2017-11-11 10:23:20 -0500

    1.fix problem in startYCSB.sh 2.redirect status to YCSB.log

--- YCSB_scripts/startYCSB.sh
+++ YCSB_scripts/startYCSB.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 workname="workloady"
 cd $YCSB_PREFIX
-bin/ycsb run hbase12 -P workloads/${workname} -cp $HBASE_PREFIX/conf -p table=usertable -p columnfamily=family -thread 4
+bin/ycsb run hbase12 -P workloads/${workname} -cp $HBASE_PREFIX/conf -p table=usertable -p columnfamily=family -threads 4 -s | tee -a /usr/local/pci_experiment_volume/YCSB.log
 cd -
 echo "run ${workname}"
 cp /usr/local/YCSB.log /usr/local/pci_experiment_volume/YCSB_throughput.log && \
