f299fa7c9080083e4788e650b270f4ca4d36b83a
Luoshang Pan <panluoshang@live.com>
2017-11-11 12:36:19 -0500

    redirect stderr

--- YCSB_scripts/startYCSB.sh
+++ YCSB_scripts/startYCSB.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 workname="workloady"
 cd $YCSB_PREFIX
-stdbuf -o0 bin/ycsb run hbase12 -P workloads/${workname} -cp $HBASE_PREFIX/conf -p table=usertable -p columnfamily=family -threads 4 -s | tee -a /usr/local/pci_experiment_volume/YCSB_redirect.log
+stdbuf -o0 bin/ycsb run hbase12 -P workloads/${workname} -cp $HBASE_PREFIX/conf -p table=usertable -p columnfamily=family -threads 4 -s 2>&1 | tee -a /usr/local/pci_experiment_volume/YCSB_redirect.log
 cd -
 echo "run ${workname}"
 cp /usr/local/YCSB.log /usr/local/pci_experiment_volume/YCSB_throughput.log && \
