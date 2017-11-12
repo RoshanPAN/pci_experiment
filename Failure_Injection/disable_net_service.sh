c4f38d2fb55b1fc28e2ae19bee0de04ccc378a0a
pls331 <panluoshang@live.com>
2017-11-12 15:39:39 -0500

    PLS's network, disk, memory fault injection

--- Failure_Injection/disable_net_service.sh
+++ Failure_Injection/disable_net_service.sh
@@ -1,4 +1,8 @@
-sleeptime=5
-sudo systemctl stop NetworkManager.service
-sleep sleeptime
-sudo systemctl start NetworkManager.service
+sleeptime=1 
+for i in 5 10 5 10 5 1
+	do
+		sudo ifconfig eno1 down && echo "stop network" && date
+		sleep $sleeptime
+		sudo ifconfig eno1 up  && echo "recover network" && date
+		sleep $i
+	done
